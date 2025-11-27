5b0225ecad2e2a819e47ffb88199f1cd793bbaaf
Yusuf Alshalabi <yusuf.oftt@gmail.com>
2025-11-27 16:41:28 +0200

    Add test code in main to test MyString.h

--- Cpp-Library-Collection/Cpp-Library-Collection.cpp
+++ Cpp-Library-Collection/Cpp-Library-Collection.cpp
@@ -1,20 +1,178 @@
-// Cpp-Library-Collection.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "MyString.h"
+
+using namespace std;
 
 int main()
+
 {
-    std::cout << "Hello World!\n";
-}
+    MyString String1;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    MyString String2("Mohammed");
+
+    String1.Value = "Ali Ahmed";
+
+    cout << "String1 = " << String1.Value << endl;
+    cout << "String2 = " << String2.Value << endl;
+
+    cout << "Number of words: " << String1.CountWords() << endl;
+
+    cout << "Number of words: " << String1.CountWords("Fadi ahmed rateb omer") << endl;
+
+    cout << "Number of words: " <<
+        MyString::CountWords("Mohammed Saqer Abu-Hadhoud") << endl;
+
+    //----------------
+    MyString String3("hi how are you?");
+
+    cout << "String 3 = " << String3.Value << endl;
+
+    cout << "String Length = " << String3.Length() << endl;
+
+    String3.UpperFirstLetterOfEachWord();
+    cout << String3.Value << endl;
+
+    //----------------
+
+    String3.LowerFirstLetterOfEachWord();
+    cout << String3.Value << endl;
+
+    //----------------
+
+    String3.UpperAllString();
+    cout << String3.Value << endl;
+
+    //----------------
+
+    String3.LowerAllString();
+    cout << String3.Value << endl;
+
+    //----------------
+
+    cout << "After inverting a : "
+        << MyString::InvertLetterCase('a') << endl;
+
+    //----------------
+
+    String3.Value = "AbCdEfg";
+
+    String3.InvertAllLettersCase();
+    cout << String3.Value << endl;
+
+    String3.InvertAllLettersCase();
+    cout << String3.Value << endl;
+
+    //----------------
+
+    cout << "Capital Letters count : "
+        << MyString::CountLetters("Mohammed Abu-Hadhoud", MyString::CapitalLetters)
+        << endl << endl;
+
+    //----------------
+
+    String3.Value = "Welcome to Jordan";
+    cout << String3.Value << endl;
+
+    cout << "Capital Letters count :" << String3.CountCapitalLetters() << endl;
+
+    //----------------
+
+    cout << "Small Letters count :" << String3.CountSmallLetters() << endl;
+
+    //----------------
+
+    cout << "vowels count :" << String3.CountVowels() << endl;
+
+    //----------------
+
+    cout << "letter E count :" << String3.CountSpecificLetter('E', false) << endl;
+
+    //----------------
+
+    cout << "is letter u vowel? " << MyString::IsVowel('a')
+        << endl;
+
+    //----------------
+
+    cout << "Words Count" << String3.CountWords()
+        << endl;
+
+    //----------------
+
+
+    vector<string> vString;
+
+    vString = String3.Split(" ");
+
+    cout << "\nTokens = " << vString.size() << endl;
+
+    for (string& s : vString)
+    {
+        cout << s << endl;
+    }
+
+    //----------------
+
+    //Tirms
+    String3.Value = "    Mohammed Abu-Hahdoud     ";
+    cout << "\nString     = " << String3.Value;
+
+    String3.Value = "    Mohammed Abu-Hahdoud     ";
+    String3.TrimLeft();
+    cout << "\n\nTrim Left  = " << String3.Value;
+
+    //----------------
+
+    String3.Value = "    Mohammed Abu-Hahdoud     ";
+    String3.TrimRight();
+    cout << "\nTrim Right = " << String3.Value;
+
+    //----------------
+
+    String3.Value = "    Mohammed Abu-Hahdoud     ";
+    String3.Trim();
+    cout << "\nTrim       = " << String3.Value;
+
+    //----------------
+
+    //Joins
+    vector<string> vString1 = { "Mohammed","Faid","Ali","Maher" };
+
+    cout << "\n\nJoin String From Vector: \n";
+    cout << MyString::JoinString(vString1, " ");
+
+
+    string arrString[] = { "Mohammed","Faid","Ali","Maher" };
+
+    cout << "\n\nJoin String From array: \n";
+    cout << MyString::JoinString(arrString, 4, " ");
+
+    //----------------
+
+    String3.Value = "Mohammed Saqer Abu-Hahdoud";
+    cout << "\n\nString     = " << String3.Value;
+
+    String3.ReverseWordsInString();
+    cout << "\nReverse Words : " << String3.Value
+        << endl;
+
+    //---------------
+
+    String3.Value = "Mohammed Saqer Abu-Hahdoud";
+    cout << "\nReplace : " << String3.ReplaceWord("Mohammed", "Sari")
+        << endl;
+
+    //---------------
+
+    String3.Value = "This is: a sample text, with punctuations.";
+    cout << "\n\nString     = " << String3.Value;
+
+    String3.RemovePunctuations();
+    cout << "\nRemove Punctuations : " << String3.Value
+        << endl;
+
+    //---------------
+    system("pause>0");
+    return 0;
+};
