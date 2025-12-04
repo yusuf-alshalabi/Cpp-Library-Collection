8effc17b81d4a5ea524cef1959acc8d7d69b600a
Yusuf Alshalabi <yusuf.oftt@gmail.com>
2025-12-04 05:30:13 +0200

    add MyPeriod.h library

---
+++ Cpp-Library-Collection/MyPeriod.h
@@ -0,0 +1,51 @@
+
+#pragma once
+#include "MyDate.h"
+
+class clsPeriod
+{
+public:
+
+    MyDate StartDate;
+    MyDate EndDate;
+
+    clsPeriod(MyDate StartDate, MyDate DateTo)
+    {
+        this->StartDate = StartDate;
+        this->EndDate = EndDate;
+
+    }
+
+    static bool IsOverlapPeriods(clsPeriod Period1, clsPeriod Period2)
+    {
+
+        if (
+            MyDate::CompareDates(Period2.EndDate, Period1.StartDate) == MyDate::enDateCompare::Before
+            ||
+            MyDate::CompareDates(Period2.StartDate, Period1.EndDate) == MyDate::enDateCompare::After
+            )
+            return false;
+        else
+            return true;
+
+    }
+
+
+    bool IsOverLapWith(clsPeriod Period2)
+    {
+        return IsOverlapPeriods(*this, Period2);
+    }
+
+    void Print()
+    {
+        cout << "Period Start: ";
+        StartDate.Print();
+
+
+        cout << "Period End: ";
+        EndDate.Print();
+
+
+    }
+
+};
