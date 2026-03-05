df2e454b950cb4507ad0d20672cb4718d74322cf
Yusuf Alshalabi <yusuf.oftt@gmail.com>
2026-03-05 23:24:43 +0200

    Feat(MyUtil): Add character generation methods

--- Cpp-Library-Collection/MyUtil.h
+++ Cpp-Library-Collection/MyUtil.h
@@ -29,5 +29,62 @@ namespace MyLib {
             return randNum;
         }
 
+        static char GetRandomCharacter(enCharType CharType)
+        {
+
+            //updated this method to accept mixchars
+            if (CharType == MixChars)
+            {
+                //Capital/Samall/Digits only
+                CharType = (enCharType)RandomNumber(1, 3);
+
+            }
+
+            switch (CharType)
+            {
+
+            case enCharType::SamallLetter:
+            {
+                return char(RandomNumber(97, 122));
+                break;
+            }
+            case enCharType::CapitalLetter:
+            {
+                return char(RandomNumber(65, 90));
+                break;
+            }
+            case enCharType::SpecialCharacter:
+            {
+                return char(RandomNumber(33, 47));
+                break;
+            }
+            case enCharType::Digit:
+            {
+                return char(RandomNumber(48, 57));
+                break;
+            }
+            default:
+            {
+                return char(RandomNumber(65, 90));
+                break;
+            }
+            }
+        }
+
+        static  std::string GenerateWord(enCharType CharType, short Length)
+
+        {
+            std::string Word;
+
+            for (int i = 1; i <= Length; i++)
+
+            {
+
+                Word = Word + GetRandomCharacter(CharType);
+
+            }
+            return Word;
+        }
+
     };
 }
