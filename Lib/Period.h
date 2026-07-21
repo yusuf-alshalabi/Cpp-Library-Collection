d42b669e5becd9fa3032503ceea3cc99b2ff071d
Yusuf Alshalabi <yusuf.alshalabi.work@gmail.com>
2026-07-21 04:18:00 +0300

    feat(period): add equality operators (==, !=) and stream operators (<<, >>)

--- Lib/Period.h
+++ Lib/Period.h
@@ -18,6 +18,32 @@ public:
         }
     }
 
+    bool operator==(const Period& Other) const
+    {
+        return (this->StartDate == Other.StartDate && this->EndDate == Other.EndDate);
+    }
+
+    bool operator!=(const Period& Other) const
+    {
+        return !(*this == Other);
+    }
+
+    friend std::ostream& operator<<(std::ostream& Out, const Period& PeriodObj)
+    {
+        PeriodObj.Print();
+        return Out;
+    }
+
+    friend std::istream& operator>>(std::istream& In, Period& PeriodObj)
+    {
+        In >> PeriodObj.StartDate >> PeriodObj.EndDate;
+        if (Date::IsDateAfterDate(PeriodObj.StartDate, PeriodObj.EndDate))
+        {
+            Date::SwapDates(PeriodObj.StartDate, PeriodObj.EndDate);
+        }
+        return In;
+    }
+
     int PeriodLengthInDays(bool IncludeEndDay = true) const
     {
         return Date::GetDifferenceInDays(StartDate, EndDate, IncludeEndDay);
